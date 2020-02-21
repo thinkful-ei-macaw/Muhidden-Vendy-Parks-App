3dca816e5b9917b3d0986dcf1fcdc916d7e7b615
Vendy Prum <vendy@thinkful.com>
2020-02-20 20:38:46 -0800

    completed park app

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const apiKey = 'grYCjWJ9GHee0eKfpbNy6f7zhFZyWquog540sWHa'; 
-const searchURL = `https://developer.nps.gov/api/v1/places?stateCode=${stateInitials}&limit=${maxResults}&start=1&q=park&api_key=${apiKey}`;
+const searchURL = 'https://developer.nps.gov/api/v1/places';
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -8,15 +8,17 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
-function getStateInfo(query, maxResults=10) {
+function getStateInfo(stateCode, maxResults=10) {
   const params = {
-    key: apiKey,
-    q: query,
-    part: 'snippet',
-    maxResults
+    api_key: apiKey,
+    q: 'park',
+    stateCode: stateCode,
+    limit: maxResults
   };
   const queryString = formatQueryParams(params);
-  const url = searchURL + '?' + queryString + '&api_key=' + apiKey;
+  console.log(queryString);
+  const url = searchURL + '?' + queryString;
+
 
   fetch(url)
     .then(response => {
@@ -25,25 +27,34 @@ function getStateInfo(query, maxResults=10) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => console.log(JSON.stringify(responseJson)))
+    .then(responseJson => {
+      for(let i = 0; i < responseJson.data.length; i++){
+        $('#results-list').append(`<li><h3>${responseJson.data[i].title}</h3>
+              <p>${responseJson.data[i].listingdescription}</p>
+              <a href="${responseJson.data[i].url}">Link to Park</a>
+              </li>`);
+      }
+
+    })
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
 }
 
 
+
 function watchForm() {
 
   $('form').submit(event => {
     event.preventDefault();
-    const stateInitials = $('#search-term').val();
+    const stateCode = $('#search-term').val();
     const maxResults = $('#js-max-results').val();
-    console.log(stateInitials);
-    console.log(maxResults);
-    getStateInfo(stateInitials, maxResults);
-    
+    $('#search-term').val('');
+    $('#js-max-results').val('');
+    getStateInfo(stateCode, maxResults);
+    $('#results-list').html('');
   });
-  //
+ 
 }
 
 $(watchForm);
